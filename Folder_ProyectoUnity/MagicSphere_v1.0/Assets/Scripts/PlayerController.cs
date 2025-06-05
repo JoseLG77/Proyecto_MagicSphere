4be2b4869b115db141595dd8ae47dfa1851049e2
OscarST96 <ocoronelfya29@gmail.com>
2025-06-05 15:19:34 -0500

    Actualizacion del diagrama

--- Folder_ProyectoUnity/MagicSphere_v1.0/Assets/Scripts/PlayerController.cs
+++ Folder_ProyectoUnity/MagicSphere_v1.0/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,31 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private bool OnMovement;
-    [SerializeField] private float speed = 1;
+    [SerializeField] private float speed;
+    //[SerializeField] private float Vf = 10; //Velocidad Final.
+    //[SerializeField] private float TimeVf = 5; //Tiempo para que llegue a la velocidad final.
+    //[SerializeField] private float acceleration; //Aceleracion 
     [SerializeField] private float jumpForce;
 
-    private Vector2 direction;
+    //private float Vi = 0; //Velocidad inicial. 
+    //private float currentTime = 0;
     private bool isJump;
+    [SerializeField]private Vector2 direction;
     private Rigidbody rb;
+
+    /*public float CurrentTime
+    {
+        set
+        {
+            if (value > TimeVf) return;
+            currentTime = value;
+        }
+    }*/
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        //acceleration = Vf / TimeVf;
     }
 
     // Update is called once per frame
@@ -28,6 +43,17 @@ public class PlayerController : MonoBehaviour
             Vector3 dir = new Vector3(direction.x, rb.linearVelocity.y, direction.y);
             rb.AddForce(dir * speed);
         }
+        /*if (OnMovement)
+        {
+            if (currentTime < TimeVf)
+            {
+                currentTime += Time.deltaTime;
+
+                float velocidadActual = acceleration * currentTime;
+                Vector3 dir = new Vector3(direction.x * velocidadActual, rb.linearVelocity.y, direction.y * velocidadActual);
+                rb.linearVelocity = dir;
+            }
+        }*/
     }
     private void OnCollisionEnter(Collision collision)
     {
