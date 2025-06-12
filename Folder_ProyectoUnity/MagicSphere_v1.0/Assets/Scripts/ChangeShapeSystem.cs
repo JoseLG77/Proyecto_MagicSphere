1d65ff3b643bc973756d86d17be048432202ac46
OscarST96 <ocoronelfya29@gmail.com>
2025-06-12 02:07:35 -0500

    12/06/2025, 02:07. Segundo avanze en el cambio de formas.

--- Folder_ProyectoUnity/MagicSphere_v1.0/Assets/Scripts/ChangeShapeSystem.cs
+++ Folder_ProyectoUnity/MagicSphere_v1.0/Assets/Scripts/ChangeShapeSystem.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ChangeShapeSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject[] ShapePrefags;
+    private DoublyLinkedCircularList ShapeList = new DoublyLinkedCircularList();
+    private NodeShape currentShapePlayer;
     void Start()
     {
-        
+        //Primero se llena la lista con las formas que va a tener el player
+        foreach (GameObject shape in ShapePrefags)
+        {
+            GameObject shapeSave = Instantiate(shape, transform.position, Quaternion.identity);
+            shapeSave.SetActive(false);//Lo ocultamos
+            ShapeList.Add(shapeSave);
+        }
+        currentShapePlayer = ShapeList.head;
+        currentShapePlayer.currentShape.SetActive(true);//Activamos la forma inicial
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ChangeNextShape(InputAction.CallbackContext context)
     {
-        
+        if (context.performed)
+        {
+            currentShapePlayer.currentShape.SetActive(false);
+            currentShapePlayer = currentShapePlayer.next;
+            currentShapePlayer.currentShape.SetActive(true);
+        }
+    }
+    public void ChangePrevShape(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            currentShapePlayer.currentShape.SetActive(false);
+            currentShapePlayer = currentShapePlayer.prev;
+            currentShapePlayer.currentShape.SetActive(true);
+        }
     }
 }
